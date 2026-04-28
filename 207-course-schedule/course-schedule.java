8e9fcabf0fb6b2f163e3ca4f1e19bbaa514620ac
karthiksery36-web <karthiksery36@gmail.com>
2026-04-28 09:55:42 +0530

    Time: 6 ms (73.29%) | Memory: 46.9 MB (77.75%) - LeetSync

---
+++ 207-course-schedule/course-schedule.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<Integer>[] graph = new ArrayList[numCourses];
+        int[] indegree = new int[numCourses];
+
+        for (int i = 0; i < numCourses; i++) graph[i] = new ArrayList<>();
+
+        for (int[] p : prerequisites) {
+            graph[p[1]].add(p[0]);
+            indegree[p[0]]++;
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) q.offer(i);
+        }
+
+        int count = 0;
+
+        while (!q.isEmpty()) {
+            int course = q.poll();
+            count++;
+
+            for (int next : graph[course]) {
+                if (--indegree[next] == 0) {
+                    q.offer(next);
+                }
+            }
+        }
+
+        return count == numCourses;
+    }
+}
